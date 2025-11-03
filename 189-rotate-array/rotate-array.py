cd1ee2bd78620c3abf6a6c6cfd3c311efe178f51
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2025-11-03 18:04:39 +0530

    Time: 15 ms (9.89%) | Memory: 25.7 MB (7.02%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        j=0
+        new_nums=[0] * len(nums)
+        n=len(nums)
+        for i in range(n):
+                new_nums[(i+k)%n]=nums[i]
+        
+        for j in range(n):
+            nums[j]=new_nums[j]
+        
+                
+        
+        
