a80f9b9510183a53452bb4b7b34dfd659287f1a3
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2025-11-08 21:00:30 +0530

    Time: 236 ms (54.65%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -4,7 +4,6 @@ Round(Avg(a2.timestamp-a1.timestamp),3)as processing_time
 from activity a1 join
 activity a2 on
 a1.machine_id=a2.machine_id
-
 where 
 (a1.activity_type="start"
 and a2.activity_type="end" )
