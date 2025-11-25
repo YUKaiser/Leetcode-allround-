ae2236848a939f7babfaed9d7153b4d6bdd57423
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2025-11-25 17:52:11 +0530

    Time: 2050 ms (28.94%) | Memory: 13.5 MB (15.24%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        ind=[]
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i]+nums[j]==target):
+                    ind.append(i)
+                    ind.append(j)
+                    return ind
+                    break
+       
+        
