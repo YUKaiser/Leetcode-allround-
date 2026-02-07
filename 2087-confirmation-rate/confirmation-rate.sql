4afc91b31c9a90d5f944a6dd0d6aae9cb12061de
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-02-07 20:56:00 +0530

    Time: 1098 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,16 +1,5 @@
-SELECT
-    s.user_id,
-    ROUND(
-        AVG(
-            IF(c.action = 'confirmed', 1.00, 0)
-        ), 
-        2
-    ) confirmation_rate
-FROM
-    Signups s
-LEFT JOIN
-    Confirmations c
-    ON
-    s.user_id = c.user_id
-GROUP BY
-    s.user_id
+SELECT A.user_id, 
+       ROUND(IFNULL(AVG(action = 'confirmed'), 0), 2) AS confirmation_rate
+FROM Signups AS A
+LEFT JOIN Confirmations AS B ON A.user_id = B.user_id
+GROUP BY A.user_id;
