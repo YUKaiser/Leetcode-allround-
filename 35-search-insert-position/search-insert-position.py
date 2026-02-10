692867877417bc30884f1aa3877d09e021d185e4
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-02-11 00:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (62.15%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -3,28 +3,19 @@ class Solution(object):
         n=len(nums)
         low=0
         high=(n-1)
+        ind=0
         
         while (low<=high):
             mid=(low+high)//2
-            for i in range(n):
-                if(nums[i]==target):
-                    return i
-                    break
-            
-            if(nums[mid]-target==1):
-                return mid
-            elif((nums[mid]-target)==-1):
-                return mid+1
-            
-
-            elif(nums[mid]<target):
-                low=mid+1
-            else:
+            if(nums[mid]>=target):
+                ind=mid
                 high=mid-1
-        if target<nums[0]:
-            return 0
-        else:
-            return n
+            else:
+                low=mid+1
+                ind=low
+        return ind
+                
+            
         """
         :type nums: List[int]
         :type target: int
