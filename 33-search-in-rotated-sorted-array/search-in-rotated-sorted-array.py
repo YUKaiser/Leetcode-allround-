107e265363733035018bd5fb701bca073ca3c1a2
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-02-18 19:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (10.58%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
         r=-1
         for i in range(0,len(nums)):
 
@@ -12,3 +7,5 @@ class Solution(object):
                 r=i
                 return r
         return r
+       
+     
