6fb5d5329d3313678f6373d079fe6122879e43e1
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-02-25 20:08:01 +0530

    Time: 401 ms (14.61%) | Memory: 13.6 MB (50.21%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        low=1
+        high=max(piles)
+        ans=0
+        while low<=high:
+
+            mid=(low+high)//2
+            total_hour=0
+            for pile in piles:
+                total_hour=math.ceil(pile/mid)+total_hour
+        
+            if(self.calculatehour(piles,mid)<=h):
+                ans=mid
+                high=mid-1
+            else:
+                low=mid+1
+               
+        return ans
+    def calculatehour(self,piles,k):
+        total_hour=0
+        for pile in piles:
+            total_hour=(pile+k-1)//k+total_hour
+        return total_hour
+        """ 
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        
