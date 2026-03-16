17f7cd4bc31c3d07b0795a093b804cd9d4c7ca23
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-03-16 20:30:01 +0530

    Time: 339 ms (9.28%) | Memory: 15.3 MB (29.36%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.py
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.py
@@ -1,20 +1,15 @@
 class Solution(object):
     def smallestDivisor(self, nums, threshold): 
-        low=1
-        high=max(nums)
-        result=high
-        while low<=high:
-            mid=(low+high)//2
-            a=self.sum_a(nums,mid)
-            if a<=threshold:
-                result=mid
-                high=mid-1
+        l,h=1,max(nums)
+        while l<=h:
+            mid=(l+h)//2
+            if sum(math.ceil(float(x)/mid) for x in nums)>threshold:
+                l=mid+1
             else:
-                low=mid+1
-        return result
-    def sum_a(self,nums,mid):
-         return sum(math.ceil(float(x )/ mid) for x in nums)
-        
+                h=mid-1
+        return l
+       
+        # see here we write float becausw it is py2 version
 
 
         
