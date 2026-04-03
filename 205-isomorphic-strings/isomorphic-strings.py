2eb3b98eeaa1dd91c39001799e380966a062b231
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-04-03 09:16:09 +0530

    Time: 10 ms (59.52%) | Memory: 13.5 MB (52.53%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,15 +1,19 @@
 class Solution(object):
     def isIsomorphic(self, s, t):
-        dicta={}
+        dicta_s_to_t={}
+        dicta_t_to_s={}
         for i in range(len(s)):
-            if s[i] in dicta:
-                if dicta[s[i]]!=t[i]:
-                    return False
-            elif(t[i] in dicta.values()):
-                if s[i] not in dicta:
+            if s[i] in dicta_s_to_t:
+                if dicta_s_to_t[s[i]]!=t[i]:
                     return False
             else:
-                dicta[s[i]]=t[i]
+                dicta_s_to_t[s[i]]=t[i]
+            
+            if t[i] in dicta_t_to_s:
+                if dicta_t_to_s[t[i]]!=s[i]:
+                    return False
+            else:
+                dicta_t_to_s[t[i]]=s[i]
         return True
         
        
