535a2684618b3e11238fe19a68492856d41d5af1
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-04-04 20:10:51 +0530

    Time: 477 ms (76.65%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -3,8 +3,3 @@ select pr.project_id,round(avg(emp.experience_years),2) as average_years
 from Project as pr  join Employee as emp
 on pr.employee_id=emp.employee_id 
 group by pr.project_id
-/*Union
-select pr.project_id,avg(emp.experience_years) as average_years
-from Project as pr right join Employee as emp
-on pr.employee_id=emp.employee_id
-group by pr.project_id */
