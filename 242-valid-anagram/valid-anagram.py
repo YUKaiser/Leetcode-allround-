59b68ae695a4f40bf1c7f4368edc79b0ca7e0c88
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-04-05 10:07:47 +0530

    Time: 21 ms (56.03%) | Memory: 14.2 MB (16.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,4 +5,19 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return Counter(s)==Counter(t)
+        if len(s)!=len(t):
+            return False
+        dicta_s={}
+        for i in range(len(s)):
+            dicta_s[s[i]]=dicta_s.get(s[i],0)+1
+        
+        for cr in t:
+            if cr not in dicta_s:
+                return False
+            else:
+                if dicta_s[cr]==0:
+                    return False
+                else:
+                    dicta_s[cr] -=1
+        
+        return True
