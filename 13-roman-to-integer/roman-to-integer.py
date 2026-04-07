e4906cf733e688af5c58288e93f3696cbe09f030
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-04-07 21:01:36 +0530

    Time: 4 ms (88.56%) | Memory: 12.3 MB (70.08%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,17 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        sum=0
-        i=0
-        dicta_s = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9,'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 
-                    'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
-        while i<len(s):
-            if s[i:i+2] in dicta_s:
-
-                sum=dicta_s[s[i:i+2]]+sum
-                i=i+2
-            else:
-                sum=dicta_s[s[i]]+sum
-                i=i+1
+        total = 0
+        i = 0
+        # Your dictionary is perfect for this approach
+        dicta_s = {
+            'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 
+            'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 
+            'CM': 900, 'M': 1000
+        }
         
-        return sum
+        while i < len(s):
+            # 1. Try to grab a 2-character slice (e.g., "IV")
+            # 2. Check if it's in our dictionary
+            if i + 1 < len(s) and s[i:i+2] in dicta_s:
+                total += dicta_s[s[i:i+2]]
+                i += 2  # Jump forward 2 steps because we used 2 characters
+            else:
+                # 3. Otherwise, just process the single character
+                total += dicta_s[s[i]]
+                i += 1  # Move forward 1 step
+                
+        return total
