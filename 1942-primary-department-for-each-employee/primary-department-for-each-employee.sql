ed307d223a6544038da1d446d01601695c61b5f5
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-04-29 20:07:00 +0530

    Time: 624 ms (15.28%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,15 +1,5 @@
-# partition - to keep all rows and then filter out
-
-
-SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-
-UNION
-
-SELECT employee_id, department_id
-FROM (
-    SELECT *, COUNT(*) OVER (PARTITION BY employee_id) as dept_count
-    FROM Employee
-) AS cte
-WHERE dept_count = 1;
+(select employee_id,department_id 
+from employee where primary_flag='Y')
+Union
+(Select employee_id,department_id
+from employee group by employee_id having count(department_id)=1)
