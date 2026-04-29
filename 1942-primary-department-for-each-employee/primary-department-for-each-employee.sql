bd2076e321feecd1bf270e0b08b7a10558971977
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-04-29 20:07:41 +0530

    Time: 1322 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,5 +1,8 @@
-(select employee_id,department_id 
-from employee where primary_flag='Y')
-Union
-(Select employee_id,department_id
-from employee group by employee_id having count(department_id)=1)
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag='Y' OR 
+    employee_id in
+    (SELECT employee_id
+    FROM Employee
+    Group by employee_id
+    having count(employee_id)=1)
