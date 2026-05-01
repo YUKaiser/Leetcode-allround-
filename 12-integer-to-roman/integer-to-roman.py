f8d290417e279d65ee701ff561795c0c03b26238
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-05-02 00:20:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.34%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,15 +1,19 @@
-class Solution(object):
-    def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
-        Hash_table={1:'I',4:'IV',5:"V",9:"IX",10:"X",40:"XL",50:"L",90:"XC",100:"C",400:"CD",500:"D",900:"CM",1000:"M"}
-        key_list=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        final=""
-        for key in key_list:
-            while num>=key:
-                final+=Hash_table[key]
-                num=num-key
-            
-        return final
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        value_symbols = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+        
+        res = []
+
+        for value, symbol in value_symbols:
+            if num == 0:
+                break
+            count = num // value
+            res.append(symbol * count)
+            num -= count * value
+
+        return ''.join(res)   
+        
