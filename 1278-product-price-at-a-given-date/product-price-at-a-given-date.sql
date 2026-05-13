4673918d64505fcbd18d77cbe9797c199ec5e1af
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-05-13 13:35:26 +0530

    Time: 453 ms (78.37%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,10 +1,11 @@
-/*select product_id,if(change_date <= '2019-08-16',new_price,10) as price
-from (select * from products order by new_price d
-*/
+
 select product_id,new_price as price 
 from products where (product_id,change_date)
-in (select product_id,max(change_date) from products where change_date<='2019-08-16' group by product_id )
+in 
+(select product_id,max(change_date) from products where change_date<='2019-08-16' group by product_id )
+
 union
+
 select product_id,10 as price from products 
 where product_id not in(
     select product_id from products where change_date <='2019-08-16' 
