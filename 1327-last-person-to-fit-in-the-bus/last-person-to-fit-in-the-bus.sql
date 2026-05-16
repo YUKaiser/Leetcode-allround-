741aecf8c9d0d5b18e41e37f1c3eb19f173db844
YUKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-05-16 09:59:06 +0530

    Time: 773 ms (74.03%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -2,4 +2,4 @@
 select person_name from 
 (select turn,person_name,sum(weight) over (order by turn) as Total_weight
 from queue ) as f where Total_weight<=1000 
-order by turn desc limit 1
+order by Total_weight desc limit 1
