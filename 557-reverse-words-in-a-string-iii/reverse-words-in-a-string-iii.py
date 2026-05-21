8ded2314c2e6492ddd0d87d33076c635af2fef11
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-05-21 10:34:28 +0530

    Time: 35 ms (7.98%) | Memory: 20.1 MB (9.89%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -1,14 +1,16 @@
-class Solution(object):
-    def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        a=s.split()
-        b=" "
-        for i in range(len(a)):
-            a[i]=a[i][::-1]
-            b=b+" "+a[i]
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        result = []
+        last_space_index = -1
 
-        return b.strip()
-        
+        for str_index in range(len(s)):
+            if str_index == len(s) - 1 or s[str_index] == ' ':
+                reverse_str_index = str_index if str_index == len(s) - 1 else str_index - 1
+                while reverse_str_index > last_space_index:
+                    result.append(s[reverse_str_index])
+                    reverse_str_index -= 1
+                if str_index != len(s) - 1:
+                    result.append(' ')
+                last_space_index = str_index
+
+        return ''.join(result)
