d4b55028cf80eb80d22695145a9bfc7a6a89a1b8
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-05-27 10:25:00 +0530

    Time: 17 ms (44.12%) | Memory: 12.3 MB (87.82%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortSentence(self, s):
+        r=s.split()
+        ans=[""]*len(r)
+        for i in range(0,len(r)):
+            a=int(r[i][-1])-1
+            ans[a]=r[i][:-1]
+        return " ".join(ans)
+
+        
