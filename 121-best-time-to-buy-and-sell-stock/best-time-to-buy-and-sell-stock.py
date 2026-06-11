3e07d0b7a566a283541a703dc3ab8e5f30ac763a
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-06-11 11:15:43 +0530

    Time: 168 ms (5.27%) | Memory: 19.4 MB (27.91%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,10 @@
 class Solution(object):
     def maxProfit(self, prices):
-        maxprofit=0
-        n=len(prices)
-        min_cost=float('inf')
-        for i in range(n):
-            min_cost=min(min_cost,prices[i])
-            if min_cost<=prices[i]:
-                maxprofit=max(maxprofit,prices[i]-min_cost)
-            
-        return maxprofit
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+        result=0
+        mi_n=float('inf')
+        for i in range(len(prices)):
+            mi_n=min(mi_n,prices[i])
+            if mi_n<=prices[i]:
+                result=max(prices[i]-mi_n,result)
+        return result
         
