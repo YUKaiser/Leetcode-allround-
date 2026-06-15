c694eb3e31562c02d6b1809d74862e7e9f6fbccf
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-06-15 20:50:57 +0530

    Time: 320 ms (56.73%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,9 +1,7 @@
 # Write your MySQL query statement be
-select id ,sum(con) as num from
-(select requester_id as id
-,count(requester_id) as con from RequestAccepted 
-group by requester_id 
-Union all
-select accepter_id as id,count(accepter_id) as con from RequestAccepted
-group by accepter_id ) as t1  group by id
+select id,count(*) as num from 
+(select requester_id as id from RequestAccepted
+Union ALL
+select accepter_id from RequestAccepted) as t1
+group by id
 order by num desc limit 1
