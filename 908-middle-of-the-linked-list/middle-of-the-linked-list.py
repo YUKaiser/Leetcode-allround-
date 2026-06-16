1cb6bce3a6d18b89179b47a1e22f32901dcf7572
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-06-16 22:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (24.17%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,17 +1,14 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def middleNode(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        slow=fast=head
+        # 1. slow and fast pointer(tortoise and hare)
+        # -here what we are doing is we are moving the fast pointer twice as fast as the slow pointer,
+        #  so when the fast pointer reaches the end of the list, the slow pointer will be at the middle of the list.)
+        slow=fast=head  
         while fast!=None and fast.next!=None:
             slow=slow.next
             fast=fast.next.next
         return slow
-        
