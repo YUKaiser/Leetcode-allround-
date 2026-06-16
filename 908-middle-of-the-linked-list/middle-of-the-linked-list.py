fc2278158bfb6c3b188d4afbd38698483a307d08
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-06-16 21:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.90%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,24 +1,19 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def middleNode(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        count=0
-        curr=head
+        count=0 #to count the number of nodes in the linked list
+        curr=head  #to traverse the linked list
         while curr is not None:
             count+=1
             curr=curr.next
         
-        curr1=head
-        trav=count//2
+        curr1=head #to traverse the linked list again to reach the middle node
+        trav=count//2 #to calculate the number of nodes to traverse to reach the middle node
         i=0
-        while i<trav:
+        while i<trav: #to traverse the linked list until we reach the middle node
             curr1=curr1.next
             i+=1
-        return curr1
+        return curr1 #to return the middle node of the linked list
