6a76d94fdf4659a0986a8cf73531ea8155ad250d
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-06-18 10:18:08 +0530

    Time: 47 ms (7.53%) | Memory: 19.6 MB (15.36%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -1,16 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
     def hasCycle(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        curr=head
-        dic={}
-        count=0
-        while curr is not None and curr.next!=None:
-            if curr not in dic:
-                dic[curr]=dic.get(curr.val)
-            else:
+        slow=fast=head
+        while fast is not None and fast.next is not None:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
                 return True
-            curr=curr.next
+        
         return False
