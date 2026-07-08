7a10861d0343504537451608b0cc9d68480aaf29
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-07-08 13:23:14 +0530

    Time: 223 ms (7.71%) | Memory: 25.8 MB (5.68%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -2,49 +2,41 @@ class MinStack(object):
 
     def __init__(self):
         self.items=[]
-        self.mini=float('inf')
+        
 
     def push(self, value):
         """
         :type value: int
         :rtype: None
         """
-        self.items.append(value)
-        self.mini=min(value,self.mini)
+        if len(self.items)==0:
+            self.items.append([value,value])
+        else:
+            mini=min(value,self.items[-1][1])
+            self.items.append([value,mini])
+        
 
     def pop(self):
         """
         :rtype: None
         """
-        if self.Empty():
-            return -1
-        self.a=self.items.pop()
-        if self.Empty():
-            self.mini=float('inf')
-        elif self.a==self.mini:
-            self.mini=min(self.items)
-
-        return self.a
-
+        if len(self.items)!=0:
+            return self.items.pop()
+        
     def top(self):
         """
         :rtype: int
         """
-        if self.Empty():
-            return 0
-        return self.items[-1]
+        if len(self.items)!=0:
+            return self.items[-1][0]
         
 
     def getMin(self):
         """
         :rtype: int
-     
         """
-        if self.Empty():
-            return -1
-        return self.mini
-    def Empty(self):
-        return len(self.items)==0   
+        if len(self.items)!=0:
+            return self.items[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
