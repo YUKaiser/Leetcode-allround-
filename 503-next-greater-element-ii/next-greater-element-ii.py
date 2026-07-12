2b1efefeee95a4cd05f48939f1754b6ca505b209
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-07-12 12:39:06 +0530

    Time: 46 ms (33.12%) | Memory: 14.3 MB (13.17%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -5,18 +5,16 @@ class Solution(object):
         :rtype: List[int]
         """
         stk=[]
-        nums1=nums+nums
-        dicta={}
-        for i in range(len(nums1)-1,-1,-1):
-            while stk and nums1[i]>=stk[-1]:
+        n=len(nums)
+        result=[-1]*n
+        for i in range(2*n-1,-1,-1):
+            while stk and nums[i%n]>=stk[-1]:
                 stk.pop()
-            if stk:
-                dicta[i]=stk[-1]
-            else:
-                dicta[i]=-1
-            stk.append(nums1[i])
-        final=[0]*len(nums)
-        for j in range(len(nums)):
-            final[j]=dicta[j]
-        return final
+            if i<n:
+                
+                if stk:
+                    result[i]=stk[-1]
+            stk.append(nums[i%n])
+
+        return result
         
