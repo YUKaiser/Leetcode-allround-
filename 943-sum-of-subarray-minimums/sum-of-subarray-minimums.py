6b2bbc4e7da78bacbd311a3e1cd23b9aa208fd79
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-07-16 20:43:34 +0530

    Time: 873 ms (24.48%) | Memory: 16.6 MB (19.73%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -1,39 +1,34 @@
 class Solution(object):
     def sumSubarrayMins(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
         MOD = 10**9 + 7
-        n = len(arr)
-
-        NSL = [-1] * n
-        NSR = [n] * n
-
-        L_stack = []
-        R_stack = []
-
-        # Previous Smaller (Strictly Smaller)
-        for i in range(n):
-            while L_stack and arr[L_stack[-1]] > arr[i]:
+        NSL=[-1]*len(arr)
+        NSR=[len(arr)]*len(arr)
+        L_stack=[]
+        R_stack=[]
+        for i in range(len(arr)):
+            while L_stack and arr[L_stack[-1]]>arr[i]:
                 L_stack.pop()
-
             if L_stack:
-                NSL[i] = L_stack[-1]
-
+                NSL[i]=L_stack[-1]
             L_stack.append(i)
-
-        # Next Smaller or Equal
-        for i in range(n - 1, -1, -1):
-            while R_stack and arr[i] <= arr[R_stack[-1]]:
+        NSR=[len(arr)]*len(arr)
+        R_stack=[]
+        for j in range(len(arr)-1,-1,-1):
+            while R_stack and arr[j]<=arr[R_stack[-1]]:
                 R_stack.pop()
-
             if R_stack:
-                NSR[i] = R_stack[-1]
+                NSR[j]=R_stack[-1]
+            R_stack.append(j)
+        mini_sum=0
+        for i in range(len(arr)):
+            left_side_min=i-NSL[i]
+            right_side_min=NSR[i]-i
+            mini_sum=(mini_sum+left_side_min*right_side_min*arr[i])%MOD
+        return mini_sum
 
-            R_stack.append(i)
+        
 
-        ans = 0
-
-        for i in range(n):
-            left = i - NSL[i]
-            right = NSR[i] - i
-            ans = (ans + left * right * arr[i]) % MOD
-
-        return ans
