643d9616dbbd8d32823cc638c5e08ab7a462dc57
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-07-17 10:42:50 +0530

    Time: 39 ms (57.20%) | Memory: 12.7 MB (4.92%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
@@ -4,14 +4,49 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxi=float("-inf")
-        mini=float("inf")
-        range_s=0
+        NMaxL=[-1]*len(nums)
+        NMaxR=[len(nums)]*len(nums)
+        left_st=[]
+        right_st=[]
+        Maxi_sum=0
         for i in range(len(nums)):
-            maxi=nums[i]
-            mini=nums[i]
-            for j in range(i,len(nums)):
-                maxi=max(maxi,nums[j])
-                mini=min(mini,nums[j])
-                range_s+=(maxi-mini)
-        return range_s        
+            while left_st and nums[left_st[-1]]<=nums[i]:
+                left_st.pop()
+            if left_st:
+                NMaxL[i]=left_st[-1]
+            left_st.append(i)
+        for i in range(len(nums)-1,-1,-1):
+            while right_st and nums[i]>nums[right_st[-1]]:
+                right_st.pop()
+            if right_st:
+                NMaxR[i]=right_st[-1]
+            right_st.append(i)
+        
+        for i in range(len(nums)):
+            left=i-NMaxL[i]
+            right=NMaxR[i]-i
+            Maxi_sum=(Maxi_sum+(left*right*nums[i]))
+
+        NML=[-1]*len(nums)
+        NMR=[len(nums)]*len(nums)
+        l_st=[]
+        r_st=[]
+        Mini_sum=0
+        for i in range(len(nums)):
+            while l_st and nums[l_st[-1]]>=nums[i]:
+                l_st.pop()
+            if l_st:
+                NML[i]=l_st[-1]
+            l_st.append(i)
+        for i in range(len(nums)-1,-1,-1):
+            while r_st and nums[i]<nums[r_st[-1]]:
+                r_st.pop()
+            if r_st:
+                NMR[i]=r_st[-1]
+            r_st.append(i)
+        
+        for i in range(len(nums)):
+            left=i-NML[i]
+            right=NMR[i]-i
+            Mini_sum=(Mini_sum+(left*right*nums[i]))
+        return Maxi_sum-Mini_sum
