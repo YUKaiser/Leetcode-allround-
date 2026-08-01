148bcb73c46e1882be08ddac99f08a762a4859df
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-08-01 08:44:03 +0530

    Time: 51 ms (8.44%) | Memory: 15.3 MB (17.75%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -5,14 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = 0
-        sums = 0
-        d = dict()
-        d[0] = 1
-        
+        dicta={}
+        sumi=0
+        result=0
+        dicta[0]=1
         for i in range(len(nums)):
-            sums += nums[i]
-            count += d.get(sums-k,0)
-            d[sums] = d.get(sums,0) + 1
-        
-        return(count)
+            sumi+=nums[i]
+            
+            if (sumi-k) in dicta:
+                result+=dicta.get(sumi-k)
+                
+            dicta[sumi]=dicta.get(sumi,0)+1
+        return result
