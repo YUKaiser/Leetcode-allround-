5f0aa52aac5ff4809b9cc4b901a47d1c33705d70
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-08-04 20:43:39 +0530

    Time: 31 ms (28.82%) | Memory: 21.4 MB (5.85%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -5,28 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        n = len(cardPoints)
-
-        if k == n:
+        if k==len(cardPoints):
             return sum(cardPoints)
-
-        left_pre = [0] * (k + 1)
-        right_pre = [0] * (k + 1)
-
-        left_sum = 0
-        right_sum = 0
-
-        for i in range(1, k + 1):
-            left_sum += cardPoints[i - 1]
-            left_pre[i] = left_sum
-
-        for i in range(1, k + 1):
-            right_sum += cardPoints[n - i]
-            right_pre[i] = right_sum
-
-        sumi = 0
-
-        for i in range(k + 1):
-            sumi = max(sumi, left_pre[i] + right_pre[k - i])
-
-        return sumi
+        result=cardPoints+cardPoints
+        i=len(cardPoints)-k
+        j=len(cardPoints)-1
+        sumi=sum(result[i:j+1])
+        it=k
+        maxi=sumi
+        while it>0:
+            sumi-=result[i]
+            i+=1
+            j+=1
+            sumi+=result[j]
+            maxi=max(maxi,sumi)
+            it-=1
+        return maxi
