25a1b61e340f40daad6cb788325894d63b9a11b0
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-08-05 10:49:54 +0530

    Time: 1 ms (91.75%) | Memory: 12.3 MB (50.52%) - LeetSync

--- 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.py
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.py
@@ -6,13 +6,14 @@ class Solution(object):
         :rtype: int
         """
         cnt=0
-        for i in range(len(s)):
-            dicta={'0':0,'1':0}
-            for j in range(i,len(s)):
-                dicta[s[j]]=dicta.get(s[j])+1
-                if dicta['0']>k and dicta['1']>k:
-                    break
-                cnt+=1
+        i=0
+        dicta={'1':0,'0':0}
+        for j in range(len(s)):
+            dicta[s[j]]=dicta.get(s[j])+1
+            while dicta['0']>k and dicta['1']>k:
+                dicta[s[i]]-=1
+                i+=1
+            cnt+=j-i+1
         return cnt
-        
+
         
