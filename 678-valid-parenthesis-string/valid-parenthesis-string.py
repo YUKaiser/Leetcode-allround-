6aba19171e872f12e6768fb32ca9ab05258fa80b
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-08-08 09:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (54.90%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -4,28 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        if s[0]==')':
-            return False
-        left_stk=[]
-        star_stk=[]
+        cnt_left=0
+        cnt_right=0
         for i in range(len(s)):
-            if s[i]==")":
-                if len(left_stk)!=0:
-                    left_stk.pop()
-                elif len(star_stk)!=0:
-                    star_stk.pop()
-                else:
-                    return False
-            elif s[i]=="*":
-                star_stk.append(i)
+            if s[i]=='(' or s[i]=="*":
+                cnt_left+=1
             else:
-                left_stk.append(i)
-        while left_stk and star_stk:
-            if left_stk[-1]>star_stk[-1]:
-                return False
-
-            star_stk.pop()
-            left_stk.pop()
-        if len(left_stk)!=0:
-            return False
-        return True
+                cnt_left-=1
+                if cnt_left<0:
+                    return False
+        for i in range(len(s)-1,-1,-1):
+            if s[i]==')' or s[i]=="*":
+                cnt_right+=1
+            else:
+                cnt_right-=1
+                if cnt_right<0:
+                    return False
+        return True    
