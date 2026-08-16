dd26398e9e451f28e8c7ac166b5c57e828317d6c
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-08-16 19:54:54 +0530

    Time: 11 ms (55.81%) | Memory: 16.1 MB (50.16%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -4,13 +4,20 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-        a=sorted(intervals)
-        i=0
-
-        while i<(len(a)-1):
-            if a[i][1]>=a[i+1][0]:
-                a[i]=[a[i][0],max(a[i][1],a[i+1][1])]
-                a.pop(i+1)
+        intervals.sort(key=lambda x:x[0])
+        newInterval=intervals[0]
+        res=[]
+        for i in range(1,len(intervals)):
+            if  newInterval[1]>=intervals[i][0]:
+                newInterval=[min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
+    
             else:
-                i+=1
-        return a
+                res.append(newInterval)
+                newInterval=intervals[i]
+        if newInterval not in res:
+            res.append(newInterval) 
+            res.sort()
+            return res
+        else:
+            return res
+                
