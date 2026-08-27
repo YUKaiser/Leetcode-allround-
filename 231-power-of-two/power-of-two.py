02042d7794161979724f14b7b3dfdc126bc7674d
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-08-27 21:23:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (89.87%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -4,11 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        cnt=0
-        while n>0:
-            if n%2==1:
-                cnt+=1
-            n=n//2
-        if cnt==1:
+        if n>0 and (n & n-1)==0:
             return True
         return False
