20a731278f12e566ce373116e8bdc1863d5b468b
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-09-13 22:00:36 +0530

    Time: 2074 ms (26.24%) | Memory: 23 MB (53.39%) - LeetSync

--- 4328-count-shadow-pairs-i/count-shadow-pairs-i.py
+++ 4328-count-shadow-pairs-i/count-shadow-pairs-i.py
@@ -1,20 +1,27 @@
-from bisect import bisect_left
 class Solution(object):
     def shadowPairs(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        cnt=0
-        stk=[]
-        
+        cnt = 0
+        stk = []
+
         for i in range(len(nums)):
-            while stk and nums[i]<stk[-1]:
+
+            while stk and nums[i] < stk[-1]:
                 stk.pop()
-            #if stk and nums[i]>stk[-1]:
-            cnt+=bisect_left(stk,nums[i])
-            
-            
+
+            # find how many elements in stk are < nums[i]
+            l = 0
+            r = len(stk)
+
+            while l < r:
+                mid = (l + r) // 2
+
+                if stk[mid] < nums[i]:
+                    l = mid + 1
+                else:
+                    r = mid
+
+            cnt += l
+
             stk.append(nums[i])
+
         return cnt
-        
