6b2c69d5084beb2efd17b7c29dc03ec75cddd7a3
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-09-16 09:20:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (45.99%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -8,20 +8,21 @@ class Solution(object):
         res=-1
         low1=0
         high1=nums.index(min(nums))
-        res=self.bsearch(nums,target,low1,high1-1,res)
+        res=self.bsearch(nums,target,low1,high1-1)
         low2=high1
         high2=len(nums)-1
-        res=self.bsearch(nums,target,low2,high2,res)
+        if res==-1:
+            res=self.bsearch(nums,target,low2,high2)
         return res
-    def bsearch(self,nums,target,low,high,res):
+    def bsearch(self,nums,target,low,high):
 
         while low<=high:
             mid=(low+high)//2
             if target==nums[mid]:
-                res=mid
+                return mid
 
             if target<nums[mid]:
                 high=mid-1
             else:
                 low=mid+1
-        return res
+        return -1
