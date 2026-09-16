3117ba37704a005a819fa483b963231f27424657
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-09-16 09:17:51 +0530

    Time: 1 ms (8.85%) | Memory: 12.6 MB (45.99%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,20 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        dicta={}
-        for i in range(len(nums)):
-            dicta[nums[i]]=i
-        a=sorted(nums)
         res=-1
-        low=0
-        n=len(nums)
-        high=n-1
+        low1=0
+        high1=nums.index(min(nums))
+        res=self.bsearch(nums,target,low1,high1-1,res)
+        low2=high1
+        high2=len(nums)-1
+        res=self.bsearch(nums,target,low2,high2,res)
+        return res
+    def bsearch(self,nums,target,low,high,res):
+
         while low<=high:
             mid=(low+high)//2
-            if target==a[mid]:
-                res=dicta[a[mid]]
+            if target==nums[mid]:
+                res=mid
 
-            if target<a[mid]:
+            if target<nums[mid]:
                 high=mid-1
             else:
                 low=mid+1
