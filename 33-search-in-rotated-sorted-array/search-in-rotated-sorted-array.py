14d20bc224c698df4fe6b64556967f00c020fcde
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-09-16 09:01:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (10.06%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,23 +1,25 @@
 class Solution(object):
     def search(self, nums, target):
-        
-        result=self.binarySearch(nums,nums.index(min(nums)),len(nums)-1,target)
-        if(result==-1):
-            result=self.binarySearch(nums,0,nums.index(min(nums))-1,target)
-        return result
-    def binarySearch(self,nums,low,high,target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        dicta={}
+        for i in range(len(nums)):
+            dicta[nums[i]]=i
+        a=sorted(nums)
+        res=-1
+        low=0
+        n=len(nums)
+        high=n-1
         while low<=high:
             mid=(low+high)//2
-            if(nums[mid]==target):
-                
-                return mid
-            elif(nums[mid]>target):
+            if target==a[mid]:
+                res=dicta[a[mid]]
+
+            if target<a[mid]:
                 high=mid-1
             else:
                 low=mid+1
-        return -1
-        
-  
-
-       
-     
+        return res
