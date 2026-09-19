035df9630884f0b5fa5650718795f9892d6d6462
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-09-20 00:06:52 +0530

    Time: 4 ms (49.59%) | Memory: 12.5 MB (20.14%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,18 +1,19 @@
 class Solution(object):
     def mySqrt(self, x):
-        low=1
-        high=x
-        r=0
-        while low<=high:
-            mid=(low+high)//2
-            if (mid*mid>x):
-                high=mid-1
-            else:
-                r=mid
-                low=mid+1
-        return r
         """
         :type x: int
         :rtype: int
         """
-        
+        if x==1:
+            return 1
+        low=1
+        high=x
+        ans=0
+        while low<=high:
+            mid=(low+high)//2
+            if mid*mid>x:
+                high=mid-1
+            else:
+                ans=mid
+                low=mid+1
+        return ans
