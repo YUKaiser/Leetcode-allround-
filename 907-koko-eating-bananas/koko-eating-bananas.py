adb91b1a920606b17998b0f4c6268be99ac99abc
FlameKaiser <166174381+YUKaiser@users.noreply.github.com>
2026-09-22 00:38:35 +0530

    Time: 391 ms (12.16%) | Memory: 13.5 MB (50.53%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,26 +1,27 @@
+import math
 class Solution(object):
     def minEatingSpeed(self, piles, h):
-        low=1
-        high=max(piles)
-        ans=0
-        while low<=high:
-
-            mid=(low+high)//2
-            total_hour=0
-            for pile in piles:
-                total_hour=(pile+mid-1)//mid+total_hour
-        
-            if(total_hour<=h):
-                ans=mid
-                high=mid-1
-            else:
-                low=mid+1
-               
-        return ans
- 
-        """ 
+        """
         :type piles: List[int]
         :type h: int
         :rtype: int
         """
+        if len(piles)==h:
+            return max(piles)
+        low=1
+        high=max(piles)
+        mini=float('inf')
+        while low<=high:
+            mid=(low+high)//2
+            if self.calculatespeed(piles,h,mid) <=h:
+                mini=min(mini,mid)
+                high=mid-1
+            else:
+                low=mid+1
+        return mini
+    def calculatespeed(self,piles,h,mid):
+        t_h=0
+        for pile in piles:
+            t_h+=math.ceil(float(pile)/mid)
+        return t_h
         
